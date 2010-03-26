cb9c298febbee5525736bbb27ae29655de8b58a4
Edoardo Batini <eodbat@gmail.com>
2010-03-26 18:12:40 +0100

    Move out blog from /section path

--- lib/blog-0.1/src/blog_admin.erl
+++ lib/blog-0.1/src/blog_admin.erl
@@ -5,7 +5,7 @@ do_add(_Args) ->
   case validate_tool:validate_cu(post, create) of
     {ok, Post} ->
       wtype_post:create(Post),
-      {redirect, "/section/blog/"};
+      {redirect, "/blog/"};
     {error, _Reason} ->
       wpart:fset("__edit", wtype_post:prepare_validated()),
       {template, "post/add.html"}
@@ -14,5 +14,5 @@ do_add(_Args) ->
 delete(Args) ->
   Id = list_to_integer(proplists:get_value(id, Args)),
   wtype_post:delete(Id),
-  {redirect, "/section/blog/"}.
+  {redirect, "/blog/"}.
 
